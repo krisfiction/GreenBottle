e64c0e1416440886dc1ecdc5f4a04eba74a0a398
krisfiction <krisfiction@gmail.com>
2021-02-27 15:14:33 -0500

    added player class setup

---
+++ GreenBottle/Characters/Character.cs
@@ -0,0 +1,25 @@
+﻿namespace GreenBottle.Characters
+{
+    public class Character
+    {
+        public string Name;
+        public string Icon;
+
+        public int X;
+        public int Y;
+
+        public int Health { get; set; }
+
+        public int HealthMax { get; set; }
+
+        public int Gold { get; set; }
+
+        public int WeaponDamageLow { get; set; }
+        public int WeaponDamageHigh { get; set; }
+        public int WeaponDamage { get; set; }
+
+        public string WeaponName { get; set; }
+
+        // add inventory here ??
+    }
+}
