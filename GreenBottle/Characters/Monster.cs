702b9288cf3e7ab0403162a60793e755c2cea647
krisfiction <krisfiction@gmail.com>
2021-02-28 10:40:51 -0500

    monsters are on the screen

---
+++ GreenBottle/Characters/Monster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenBottle.Characters
+{
+    public class Monster : Character
+    {
+        public static Random random = new Random();
+
+        // values for random health based on high/low - so monsters will have varying health
+        public int HealthLow { get; set; }
+        public int HealthHigh { get; set; }
+
+        public int SetHealth()
+        {
+            //Health = random.Next(HealthLow, HealthHigh);
+            //HealthMax = Health;
+
+            return random.Next(HealthLow, HealthHigh);
+        }
+
+    }
+}
---
+++ GreenBottle/Characters/Monsters/Rat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenBottle.Characters.Monsters
+{
+    internal class Rat : Monster
+    {
+        public Rat()
+        {
+            Name = "Rat";
+            Icon = "R";
+            HealthLow = 10;
+            HealthHigh = 15;
+            Gold = 10;
+            WeaponDamageLow = 2;
+            WeaponDamageHigh = 10;
+        }
+    }
+}
